6c2ae9244f13c249715a4363cff178a474395a70
Pablo de la Peña <p@shedcollective.org>
2021-04-07 13:28:45 +0100

    Fix indentation

--- apache/php80/templates/ssl-create.sh
+++ apache/php80/templates/ssl-create.sh
@@ -19,9 +19,9 @@ if [[  "${SSL_DOMAIN}" != "localhost"  ]]; then
         --domain ${DOMAINS} \
         --webroot /var/www/html || exit 1
 
-   SSL_CERT="/root/.acme.sh/${SSL_DOMAIN}/${SSL_DOMAIN}.cer"
-   SSL_KEY="/root/.acme.sh/${SSL_DOMAIN}/${SSL_DOMAIN}.key"
-   SSL_CHAIN="/root/.acme.sh/${SSL_DOMAIN}/fullchain.cer"
+    SSL_CERT="/root/.acme.sh/${SSL_DOMAIN}/${SSL_DOMAIN}.cer"
+    SSL_KEY="/root/.acme.sh/${SSL_DOMAIN}/${SSL_DOMAIN}.key"
+    SL_CHAIN="/root/.acme.sh/${SSL_DOMAIN}/fullchain.cer"
 
 else
     echo "Installing self-signed certificates for localhost:"
