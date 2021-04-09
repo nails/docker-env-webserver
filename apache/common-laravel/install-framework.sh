b7d85d2e636ff95d52d58e1982fe8295e04472b8
Pablo de la Peña <p@shedcollective.org>
2021-04-09 18:23:08 +0100

    Remove unused framework installation code

--- apache/common-laravel/install-framework.sh
+++ apache/common-laravel/install-framework.sh
@@ -10,13 +10,5 @@ TARGET="/var/www/html"
 
 # --------------------------------------------------------------------------
 
-if ! [[ -x "$(command -v laravel)" ]]; then
-    echo "... installing Laravel installer"
-    composer global require "laravel/installer"
-fi
-
-# Ensure Composer bin dir is in $PATH
-export PATH="$PATH:$HOME/.composer/vendor/bin"
-
 echo "... installing Laravel"
 composer create-project laravel/laravel $TARGET
