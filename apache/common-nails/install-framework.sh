ca4b1e34a09ec0ccfd9db31ed691a48d442cc3f5
Pablo de la Peña <p@shedcollective.org>
2021-04-09 18:26:10 +0100

    nails cli tool will be installed globally

--- apache/common-nails/install-framework.sh
+++ apache/common-nails/install-framework.sh
@@ -10,13 +10,5 @@ TARGET="/var/www/html"
 
 # --------------------------------------------------------------------------
 
-if ! [[ -x "$(command -v nails)" ]]; then
-    echo "... installing Nails Command Line Tool"
-    composer global require "nails/command-line-tool"
-fi
-
-# Ensure Composer bin dir is in $PATH
-export PATH="$PATH:$HOME/.composer/vendor/bin"
-
 echo "... installing Nails"
 nails new:project --dir="$TARGET" --no-docker
